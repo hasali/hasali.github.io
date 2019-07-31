0d193c0a95325a581a62a957b51ad2fa8c02a5ec
hassan <hasalimah@gmail.com>
2019-07-31 00:00:55 -0400

    minbtn fix and git logo added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -77,8 +77,9 @@ myApp.directive('toggleClass', function() {
   return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-          element.parent().bind('click', function() {
+          element.bind('click', function() {
               element.toggleClass(attrs.toggleClass);
+              
           });
       }
   };
